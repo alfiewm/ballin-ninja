9244f774fc9e569bf3cf14b2b93609103a8868b3
alfiewm <alfiewm@gmail.com>
2014-10-13 16:16:19 +0800

    pocket login animation

---
+++ YourFavApps/src/com/mengwang/ui/activity/PocketAnimActivity.java
@@ -0,0 +1,14 @@
+package com.mengwang.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PocketAnimActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
