3687335f5f69d60351ae2947532871d6c1ea6110
mengwang <alfiewm@gmail.com>
2014-10-23 23:40:30 +0800

    More NB solution

--- MyLeetCodeAnswers/C++/ConvertSortedArrayToBST.cpp
+++ MyLeetCodeAnswers/C++/ConvertSortedArrayToBST.cpp
@@ -30,3 +30,25 @@ public:
         }
     }
 };
+
+/* more NB solution from others.
+class Solution {
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        return convert(num.begin(), num.end());
+    }
+
+    TreeNode *convert(vector<int>::iterator first, vector<int>::iterator last) {
+        const int length = distance(first, last);
+        if (length == 0) return NULL;
+        if (length == 1) return new TreeNode(*first);
+
+        vector<int>::iterator mid = first + length/2;
+        TreeNode *root = new TreeNode(*mid);
+        root->left = convert(first, mid);
+        root->right = convert(mid+1, last);
+
+        return root;
+    }
+};
+*/
