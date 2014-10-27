6d154ea0c4c5cc0602eb179d8019062cc97c1b84
alfiewm <alfiewm@gmail.com>
2014-10-27 19:29:40 +0800

    Fixed Max Depth of B tree
    use recursive and non recursive method

--- MyLeetCodeAnswers/C++/maxDepthOfBtree.cpp
+++ MyLeetCodeAnswers/C++/maxDepthOfBtree.cpp
@@ -1,38 +1,45 @@
-#include <iostream>
-#include <stack>
-using namespace std;
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+/*Recursive*/
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL) {
+            return 0;
+        } else {
+            return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        }
+    }
 };
 
 
-int maxDepthOfBtree(TreeNode *root) {
-    if (root == NULL) {
-        return 0;
-    } else if (root->left == NULL) {
-        return 1 + maxDepthOfBtree(root->right);
-    } else if (root->right == NULL) {
-        return 1 + maxDepthOfBtree(root->left);
-    } else {
-        return 1 + max(maxDepthOfBtree(root->left), maxDepthOfBtree(root->right));
+ /*None Recursive*/
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == NULL) return 0;
+        TreeNode *p = root;
+        queue<TreeNode*> que;
+        que.push(p);
+        int result(0);
+        while (!que.empty()) {
+            result++;
+            int count = que.size();
+            while(count--) {
+                p = que.front();
+                que.pop();
+                if (p->left != NULL) que.push(p->left);
+                if (p->right != NULL) que.push(p->right);
+            }
+        }
+        return result;
     }
-}
-
-
-int main() {
-    TreeNode *root = new TreeNode(0);
-    TreeNode *first = new TreeNode(1);
-    TreeNode *second = new TreeNode(2);
-    TreeNode *third = new TreeNode(3);
-    TreeNode *forth = new TreeNode(4);
-    root->left = first;
-    root->right = second;
-    second->right = third;
-    second->left = forth;
-    //forth->left=second;
-    cout << maxDepthOfBtree(root) << endl;
-    return 0;
-}
+};
