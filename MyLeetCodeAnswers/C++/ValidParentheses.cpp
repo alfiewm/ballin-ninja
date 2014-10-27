a77ece896d3f9af7f07e4b152020ab47de2498e2
alfiewm <alfiewm@gmail.com>
2014-10-27 17:15:58 +0800

    Valid Parentheses improved

--- MyLeetCodeAnswers/C++/ValidParentheses.cpp
+++ MyLeetCodeAnswers/C++/ValidParentheses.cpp
@@ -30,3 +30,33 @@ public:
         return sta.empty();
     }
 };
+
+/*Improved Version*/
+class Solution {
+public:
+    char getRight(char left) {
+        switch (left) {
+            case '(': return ')';
+            case '{': return '}';
+            case '[': return ']';
+            default: return '\0';
+        }
+    }
+    
+    inline bool isLeft(char left) {
+        return (left == '(' || left == '{' || left == '[');
+    }
+    
+    bool isValid(string s) {
+        stack<char> sta;
+        for (int i = 0; i < s.size(); ++i) {
+            if (isLeft(s[i])) {
+                sta.push(s[i]);
+            } else {
+                if (sta.empty() || getRight(sta.top()) != s[i]) return false;
+                else sta.pop();
+            }
+        }
+        return sta.empty();
+    }
+};
