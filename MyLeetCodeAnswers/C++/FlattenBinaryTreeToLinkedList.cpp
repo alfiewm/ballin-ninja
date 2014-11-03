c23f746df36c050de236618246d947445779b02a
mengwang <alfiewm@gmail.com>
2014-11-03 23:10:40 +0800

    Flatten Binary Tree To Linked List
    None recursive way

--- MyLeetCodeAnswers/C++/FlattenBinaryTreeToLinkedList.cpp
+++ MyLeetCodeAnswers/C++/FlattenBinaryTreeToLinkedList.cpp
@@ -1,3 +1,36 @@
+
+
+/*None recursive solution from Chen Hao*/
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        if (root == NULL) return;
+        vector<TreeNode*> stack, v;
+        stack.push_back(root);
+        while (!stack.empty()) {
+            TreeNode *p = stack.back();
+            v.push_back(p);
+            stack.pop_back();
+            
+            if (p->right != NULL) {
+                stack.push_back(p->right);
+            }
+            if (p->left != NULL) {
+               stack.push_back(p->left); 
+            }
+        }
+        // 精髓，加个标记位，更方便
+        v.push_back(NULL);
+        for (int i = 0; i < v.size(); ++i) {
+            if (v[i]) {
+                v[i]->left = NULL;
+                v[i]->right = v[i+1];
+            }
+        }
+    }
+};
+
+/*recursive solution by myself*/
 class Solution {
 public:
     void flatten(TreeNode *root) {
