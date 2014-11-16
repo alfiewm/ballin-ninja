7c3848bf014cf99b29904090bc918b94977ee8b7
alfiewm <alfiewm@gmail.com>
2014-11-16 21:13:19 +0800

    Add desc and more solution for validate BST

--- MyLeetCodeAnswers/C++/ValidateBinarySearchTree.cpp
+++ MyLeetCodeAnswers/C++/ValidateBinarySearchTree.cpp
@@ -1,12 +1,35 @@
-/**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+// Source   : https://oj.leetcode.com/problems/validate-binary-search-tree/
+// Authoer  : Wang Meng
+// Date     : 2014-11-16
+
+/*
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
  */
+
+// recursive solution, from Discuss area
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+        TreeNode *pre = NULL;
+        return validate(root, pre);
+    }
+    
+    // 注意形参pre类型，为指针的引用，即方法内做的修改是针对pre指针的修改，而不仅仅是pre指向数据的修改。类似逆序链表时，传入ListNode **head返回void一样
+    bool validate(TreeNode *root, TreeNode *&pre) {
+        if (root == NULL) return true;
+        if (!validate(root->left, pre)) return false;
+        if (pre!=NULL && pre->val>=root->val) return false;
+        pre = root;
+        return validate(root->right, pre);
+    }
+};
+
 
 /*早起神清气爽，递归一下就出来了，果然休息足才有动力啊，毕竟不是学霸*/
 class Solution {
