7ff96a8edeb3c1b1b33dbe44b4621f477e01987d
alfiewm <alfiewm@gmail.com>
2014-11-20 16:02:43 +0800

    B tree post order, other ideas

--- MyLeetCodeAnswers/C++/BinaryTreePostOrderTraversal.cpp
+++ MyLeetCodeAnswers/C++/BinaryTreePostOrderTraversal.cpp
@@ -16,6 +16,56 @@
  * 
  * Note: Recursive solution is trivial, could you do it iteratively?
  */
+
+// 又一个好方法，罗列了访问到一个节点时满足那些条件才应该访问，也使用了pre指针
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> result;
+        if (root == NULL) return result;
+        TreeNode *p = root;
+        TreeNode *pre = NULL;
+        stack<TreeNode*> sta;
+        sta.push(p);
+        while (!sta.empty()) {
+            p = sta.top();
+            // if pre is from substree, then it is either from right or from left.
+            if (p->right!=NULL && p->right==pre
+              || p->right==NULL && p->left==pre
+              || p->left==NULL && p->right==NULL) {
+                sta.pop();
+                result.push_back(p->val);
+                pre = p;
+                continue;
+            }
+            if (p->right != NULL) sta.push(p->right);
+            if (p->left != NULL) sta.push(p->left);
+        }
+        return result;
+    }
+};
+
+// another good idea, 使用栈，依次访问根，右子树，左子树，最终对结果逆序就可以了
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> result;
+        if (root == NULL) return result;
+        TreeNode *p = root;
+        stack<TreeNode*> sta;
+        sta.push(p);
+        while (!sta.empty()) {
+            p = sta.top();
+            result.push_back(p->val);
+            sta.pop();
+            if (p->left != NULL) sta.push(p->left);
+            if (p->right != NULL) sta.push(p->right);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
 // My none recursive solution
 class Solution {
 public:
