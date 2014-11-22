dcae6fb4de10c3a18559097901c6169c8e70ac25
mengwang <alfiewm@gmail.com>
2014-11-22 22:07:23 +0800

    More solution for Subsets

--- MyLeetCodeAnswers/C++/Subsets.cpp
+++ MyLeetCodeAnswers/C++/Subsets.cpp
@@ -23,6 +23,47 @@
  * ]
  */
 
+// recursive solution，每个元素都可以选择在不在一个子集中
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<vector<int> > res;
+        vector<int> cur;
+        subsetsR(cur, res, S, 0);
+        return res;
+    }
+    
+    void subsetsR(vector<int> &cur, vector<vector<int> > &res, vector<int> &S, int index) {
+        if (index == S.size()) res.push_back(cur);
+        else {
+            vector<int> noIndexElement = cur;
+            vector<int> hasIndexElement = cur; hasIndexElement.push_back(S[index]);
+            subsetsR(noIndexElement, res, S, index+1);
+            subsetsR(hasIndexElement, res, S, index+1);
+        }
+    }
+};
+
+// DP solution from discuss area，从0个元素，1一个元素依次推演而来
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<vector<int> > res;
+        res.push_back(vector<int>());
+        for (int i=0; i<S.size(); ++i) {
+            int size = res.size();
+            for (int j=0; j<size; ++j) {
+                res.push_back(res[j]);
+                res[size+j].push_back(S[i]);
+            }
+        }
+        return res;
+    }
+};
+
+
 // idea from discuss area, 实现思路：最终结果的个数为2的S.size()次方，对于编号为x的结果，将x看成2进制数字，从低位到高位遍历j，如果是1就将对应位置的S[j]加入到result[x]中
 class Solution {
 public:
