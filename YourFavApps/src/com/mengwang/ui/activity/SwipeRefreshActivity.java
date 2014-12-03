5080400c4f53908444b3f4f3c8224c8defa0224e
alfiewm <alfiewm@gmail.com>
2014-12-03 17:22:33 +0800

    setup list view for swipe refresh activity.

--- YourFavApps/src/com/mengwang/ui/activity/SwipeRefreshActivity.java
+++ YourFavApps/src/com/mengwang/ui/activity/SwipeRefreshActivity.java
@@ -1,6 +1,7 @@
 package com.mengwang.ui.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -13,19 +14,20 @@ import com.mengwang.guessyourfav.R;
 public class SwipeRefreshActivity extends Activity {
 
 	private SwipeRefreshLayout mRefreshLayout;
+	private String[] dataStrings = { "Bill", "Molly", "Cassie", "Pony", "Jony", "Tony", "Luny" };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_swipe_refresh);
 		mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
-		mRefreshLayout.setColorScheme(android.R.color.black, android.R.color.white, android.R.color.black,
-				android.R.color.white);
+		mRefreshLayout.setColorSchemeColors(Color.RED, Color.BLACK);
 		mRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
-			
+
 			@Override
 			public void onRefresh() {
 				new Handler().postDelayed(new Runnable() {
-					
+
 					@Override
 					public void run() {
 						mRefreshLayout.setRefreshing(false);
@@ -33,11 +35,9 @@ public class SwipeRefreshActivity extends Activity {
 				}, 2000);
 			}
 		});
-		
-		ListView list = (ListView)findViewById(R.id.swipe_list);
-		list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1) {
-			
-		});
+
+		ListView list = (ListView) findViewById(R.id.swipe_list);
+		list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataStrings));
 	}
 
 }
