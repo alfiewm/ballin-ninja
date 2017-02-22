7d8ee6d44fb10be5a363d0adc17e539a53260014
wangmeng <wangmeng@fenbi.com>
2017-02-22 12:25:00 +0800

    refile spalsh demo.

--- StatusBarTint-v2/app/src/main/java/meng/statusbartint/ui/SplashActivity.java
+++ StatusBarTint-v2/app/src/main/java/meng/statusbartint/ui/SplashActivity.java
@@ -1,6 +1,8 @@
 package meng.statusbartint.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import meng.statusbartint.R;
 import meng.statusbartint.base.BaseActivity;
@@ -9,12 +11,14 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        getWindow().getDecorView().setSystemUiVisibility(
-//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         super.onCreate(savedInstanceState);
-//        StatusBarUtil.transparencyBar(this);
-//        StatusBarUtil.setLightStatusBar(this, false);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, OmniThemeActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
