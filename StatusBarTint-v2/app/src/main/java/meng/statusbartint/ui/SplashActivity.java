66775f815d94317331819c21e095364f368226ad
wangmeng <wangmeng@fenbi.com>
2017-03-01 21:16:15 +0800

    fix splash window background.

--- StatusBarTint-v2/app/src/main/java/meng/statusbartint/ui/SplashActivity.java
+++ StatusBarTint-v2/app/src/main/java/meng/statusbartint/ui/SplashActivity.java
@@ -1,33 +1,40 @@
 package meng.statusbartint.ui;
 
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import meng.statusbartint.R;
 import meng.statusbartint.base.BaseActivity;
 
 public class SplashActivity extends BaseActivity {
 
-    @Override
-    protected int getContentLayoutResId() {
-        return R.layout.activity_splash;
-    }
-
-    @Override
-    protected boolean drawBeneathStatusBar() {
-        return true;
-    }
+    @BindView(R.id.eh_img)
+    ImageView ehImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        }
+        ButterKnife.bind(this);
+        ehImageView.setVisibility(View.GONE);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, OmniThemeActivity.class));
-                finish();
+                ehImageView.setVisibility(View.VISIBLE);
             }
         }, 2000);
+    }
+
+    @Override
+    protected int getContentLayoutResId() {
+        return R.layout.activity_splash;
     }
 }
